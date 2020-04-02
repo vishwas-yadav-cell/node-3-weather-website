679eb3cbedeedcf10c6ee62359107059ae768ee6
vishwas-yadav-cell <vishwasyadav164@gmail.com>
2020-04-02 15:28:36 +0530

    update the forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,7 @@
 const request = require('request');
 
+
+
 const forecast = (lat, long, callback) => {
     const url = `https://api.darksky.net/forecast/a607d2413359727efd887418dbec1f75/${lat},${long}?units=si`
 
@@ -9,7 +11,7 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('unable to find the location', undefined);
         } else {
-            callback(undefined, `Weather is ${body.currently.summary} and the current temprature is ${body.currently.temperature}° celcius. there will be ${body.currently.precipProbability}% chance of rain in`)
+            callback(undefined, `Weather is ${body.currently.summary} and the current temprature is ${body.currently.temperature}° celcius.the wind speed is ${body.currently.windSpeed} , there will be ${body.currently.precipProbability}% chance of rain in`)
         }
     })
 }
